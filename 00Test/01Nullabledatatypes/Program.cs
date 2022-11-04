334e82bc68f3a49aab4d83d64e49d2008e9a9879
jakob0123 <jakob.prem@josephinum.at>
2022-11-04 16:44:57 +0100

    16:44 melde nichts

---
+++ 00Test/01Nullabledatatypes/Program.cs
@@ -0,0 +1,14 @@
+﻿int i = 100;
+int? j = null;    
+// not nullable noch kein Wert zugwiesen
+Console.WriteLine(j);
+
+j = 200;
+Console.WriteLine(j);
+
+if (j.HasValue)
+{
+    i = i * j.Value;
+}
+
+

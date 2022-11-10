491b5ebf2bde3878d9a918aae1c6e9d24bec33cd
jakob0123 <jakob.prem@josephinum.at>
2022-11-10 08:54:29 +0100

    kapitel ünnötig

--- 00Test/03TryParse/Program.cs
+++ 00Test/03TryParse/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Bitte eine Zahl eingeben:");
+string input = Console.ReadLine();
+//int inputValue = int.Parse(input); möglicherweiße wenn keine Zahl kommt einen Fehler
+int number = 11;
+bool success = int.TryParse(input, out number);
+if (success)
+{
+    Console.WriteLine("Super du hast" + number + "eingegeben");
+}
+else
+{
+    //Eingabe wiederholen
+    Console.WriteLine("Leider keine Zahl.");
+}
+//double.TryParse();
+//bool.TryParse();

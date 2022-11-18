37df0569aadfed6aca0496236c140d0b4966c71e
jakob0123 <jakob.prem@josephinum.at>
2022-11-18 15:33:41 +0100

    test02

---
+++ 00Test/05Delegets/Program.cs
@@ -0,0 +1,3 @@
+﻿public delegate int Rechneroperation(int x ,int y); //Definition eines Datentyps
+                                                    //Mögliche werte: alle Funktionen oder Methoden
+                                                    //welche int als Rückgabewert habe und 2 int als Parameter

9a11601bf526d1b1def44a2c7d77d426eb364ee0
jakob0123 <jakob.prem@josephinum.at>
2023-01-13 16:29:43 +0100

    sdfasdfasdfsadf

--- 00Test/12_DownloadClient/PdfDownloader.cs
+++ 00Test/12_DownloadClient/PdfDownloader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,29 @@ namespace _12_DownloadClient
 {
     internal class PdfDownloader
     {
+        public async Task<string>DownloadAsync(Uri url)
+        {
+            #region webclient
+            using (WebClient client = new WebClient())
+            {
+                string pdfname;
+                if (!url.Segments.Last().EndsWith("pdf"))
+                {
+                    pdfname = url.Segments.Last()+".pdf";
+                }
+                else
+                {
+                    pdfname = url.Segments.Last();
+                }
+                string filename = @"C:\dev\" + pdfname;
+                Console.Write("Download:" + pdfname + "::::::::::::::::::");
+                Task t = client.DownloadFileTaskAsync(url, filename);
+                await t;
+                Console.WriteLine("Download finished: "+pdfname);
+                //hier wird automatisch ein Task Objekt zurückgeben(macht Compiler)
+                return filename;
+            }
+            #endregion
+        }
     }
 }
--- 00Test/12_DownloadClient/Program.cs
+++ 00Test/12_DownloadClient/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _12_DownloadClient;
+
+List<Uri> uris = new List<Uri>();
+uris.Add(new Uri("http://bit.ly/leseprobe_schroedinger_cplusplus"));
+uris.Add(new Uri("http://bit.ly/leseprobe_schroedinger_abap"));
+uris.Add(new Uri("http://bit.ly/leseprobe_schroedinger_html5"));
+uris.Add(new Uri("http://bit.ly/leseprobe_programmieren_lernen"));
+uris.Add(new Uri("https://www.thecodingguys.net/resources/cs-cheat-sheet.pdf"));
+
+List<Task<string>> tasks = new List<Task<string>>();
+foreach (var url in uris)
+{
+    PdfDownloader downloader = new PdfDownloader();
+    Task<string> t = downloader.DownloadAsync(url);
+    string r =t.Result;
+    tasks.Add(t);
+    Console.WriteLine(r);
+
+}
+Task.WaitAll(tasks.ToArray());
+//Warten bis alle task fertig sind 
+Console.WriteLine("Alle programme abgeschloßen");
+
+Console.ReadLine();

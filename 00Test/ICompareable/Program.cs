4ed252f8cb8f61390c9ee409f1f10ef15ebd5faa
jakob0123 <jakob.prem@josephinum.at>
2023-01-13 15:23:08 +0100

    sddd

---
+++ 00Test/ICompareable/Program.cs
@@ -0,0 +1,19 @@
+﻿using ICompareable;
+
+Student stefan = new Student() { Firstname="Stefan",Lastname="Schwödiauer",Birthday= new DateTime(2005,3,28)};
+Student gabriel = new Student() { Firstname="Gabriel",Lastname="Steinwander",Birthday = new DateTime(2005,3,29)};
+Student florian = new Student() { Firstname = "Florian", Lastname = "Winkler", Birthday = new DateTime(2005, 3, 28) };
+
+List<Student> students = new List<Student>();
+students.Add(stefan);
+students.Add(gabriel);
+students.Add(florian);
+students.Sort();
+
+students.ForEach((s) => Console.WriteLine(s));
+
+//foreach (Student s in students)
+//{
+//    Console.WriteLine(s);
+//}
+
---
+++ 00Test/ICompareable/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICompareable
+{
+    internal class Student:IComparable<Student>//ICompareable notwendig wenn ich sortieren möchte
+
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public int CompareTo(Student? other)
+        {
+            //bool lower = this.Birthday < other.Birthday;
+            //if (lower)
+            //{
+            //    return -1;
+            //}
+            //else
+            //{
+            //    return 1;
+            //}
+            return this.Birthday.CompareTo(other.Birthday);   //nachdem auch DateTime Icompareable implementiert können wir dies einfach aufrufen.
+
+
+        }
+
+        public override string ToString()
+        {
+            return Firstname + " " + Lastname + " - " + Birthday.ToShortDateString;
+        }
+    }
+}

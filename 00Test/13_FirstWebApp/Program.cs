e58a0292f8e14246e8b7cd0f0fe4bc6d4ca99763
jakob0123 <jakob.prem@josephinum.at>
2023-02-02 09:20:08 +0100

    asdasdsa

---
+++ 00Test/13_FirstWebApp/Program.cs
@@ -0,0 +1,12 @@
+using _13_FirstWebApp;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!!!");
+Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+{
+    webBuilder.UseStartup<Startup>();
+
+});
+app.Run();
---
+++ 00Test/13_FirstWebApp/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace _13_FirstWebApp
+{
+    public class Startup
+    {
+        public void ConfigureServcies(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsDevelopment())
+            {
+                app.UseHsts();
+            }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+
+
+            app.UseDefaultFiles(); //index.html , index.htm, default.html
+            app.UseStaticFiles();   //damit statische websiten behandelt werden können.
+
+            
+        }
+    }
+}

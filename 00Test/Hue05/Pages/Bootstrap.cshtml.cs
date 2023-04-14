63a3ceb4a46529a4ef6361705bb7c21826163fee
jakob0123 <jakob.prem@josephinum.at>
2023-04-14 16:08:12 +0200

    niemand nie

---
+++ 00Test/Hue05/Pages/Bootstrap.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hue05.Pages
+{
+    public class BootstrapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
